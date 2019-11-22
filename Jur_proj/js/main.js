092f1ba344ade61c65d579b16463a05038ff93d5
Jekkich87 <55157052+Jekkich87@users.noreply.github.com>
2019-11-22 17:47:25 +0200

    form finished

--- Jur_proj/js/main.js
+++ Jur_proj/js/main.js
@@ -3,4 +3,6 @@ $(function () {
         nextArrow: '<button type="button" class="slick-btn slick-next"></button>', 
         prevArrow: '<button type="button" class= "slick-btn slick-prev" ></button>',
         infinite: false});
+
+    $('select').styler();
 })
