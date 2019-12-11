835fe5bc9166c6b757e2facb3142b9c5f925c979
Jekkich87 <55157052+Jekkich87@users.noreply.github.com>
2019-12-11 15:19:12 +0200

    stylizing gamefield

--- My_TicTacToe_JS_proj/ticjs.js
+++ My_TicTacToe_JS_proj/ticjs.js
@@ -2,3 +2,10 @@ for (var i = 0; i < 9; i++){
     document.getElementById('game').innerHTML+=`<div class="block"></div>`
 }  //создаем 9 блоков для поля игры, creating 9 block for gaming field
 
+var hod = 0;
+
+document.getElementById('game').onclick = function (event) { 
+    console.log(event);
+    
+    
+} //событие клик для записи значка в клетку
