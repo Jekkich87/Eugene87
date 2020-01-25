534ec1c1bc9e048ba9667a18ab3d2f5e0df865a2
Jekkich87 <55157052+Jekkich87@users.noreply.github.com>
2020-01-25 17:49:13 +0200

    function to render list of 'todo'-lists, function to create new 'todo'-lists

--- My_JS_training_proj/ToDo_List/Todo.js
+++ My_JS_training_proj/ToDo_List/Todo.js
@@ -1,3 +1,45 @@
 const listsContainer = document.querySelector("[data-lists]");
+const newListForm = document.querySelector('#data-new-list-form');
+const newListInput = document.querySelector('#data-new-list-input');
 
-let lists = [];
+let lists = [{
+    id: 1,
+    name: 'name'
+}, {    id:2,
+        name: "todo"
+    }];
+
+function render() { //rendering list of 'todo'-lists
+    clearElement(listsContainer);
+    lists.forEach(list => {
+        const listElement = document.createElement('li');
+        listElement.dataset.listId = list.id; //marking wich list id active
+        listElement.classList.add('list-name');
+        listElement.innerText = list.name;
+        listsContainer.appendChild(listElement);
+    })
+}
+
+function clearElement(element) {
+    while (element.firstChild) {
+        element.removeChild(element.firstChild);
+    }
+}
+
+render();
+
+newListForm.addEventListener('submit', e => {//posibility to create new 'todo'-lists
+    e.preventDefault();
+    const listName = newListInput.value;
+    if (listName == null || listName === '') return;
+    const list = createList(listName);
+    newListInput.value = null;
+    lists.push(list);
+    render();
+})
+
+function createList(name) {
+    return {
+        id: Date.now().toString(), name: name, tasks: []
+    }
+}
