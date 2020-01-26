03923ea75b0111c75e15cb62fe3014900b17ef87
Jekkich87 <55157052+Jekkich87@users.noreply.github.com>
2020-01-26 11:24:14 +0200

    saving added lists in local storage and marking clicked list as active list

--- My_JS_training_proj/ToDo_List/Todo.js
+++ My_JS_training_proj/ToDo_List/Todo.js
@@ -1,13 +1,13 @@
-const listsContainer = document.querySelector("[data-lists]");
+const listsContainer = document.querySelector("#data-lists");
 const newListForm = document.querySelector('#data-new-list-form');
 const newListInput = document.querySelector('#data-new-list-input');
 
-let lists = [{
-    id: 1,
-    name: 'name'
-}, {    id:2,
-        name: "todo"
-    }];
+const LOCAl_STORAGE_LIST_KEY = 'task.list'  //prevent overwriting local storage
+const LOCAL_STORAGE_SELECTED_LIST_ID_KEY = 'task.selectedListId';
+
+let lists = JSON.parse(localStorage.getItem(LOCAl_STORAGE_LIST_KEY))||[]; //getting info from local storage or, an empty aray, if info doesn`t exist
+let selectedListId = localStorage.getItem(LOCAL_STORAGE_SELECTED_LIST_ID_KEY); //choosing list as 'active list'
+
 
 function render() { //rendering list of 'todo'-lists
     clearElement(listsContainer);
@@ -16,6 +16,9 @@ function render() { //rendering list of 'todo'-lists
         listElement.dataset.listId = list.id; //marking wich list id active
         listElement.classList.add('list-name');
         listElement.innerText = list.name;
+        if (list.id === selectedListId) {
+            listElement.classList.add('active-list') //adding class"active-list" to the choosen list
+        }
         listsContainer.appendChild(listElement);
     })
 }
@@ -28,18 +31,36 @@ function clearElement(element) {
 
 render();
 
-newListForm.addEventListener('submit', e => {//posibility to create new 'todo'-lists
+newListForm.addEventListener('submit', e => { //posibility to create new 'todo'-lists
     e.preventDefault();
     const listName = newListInput.value;
     if (listName == null || listName === '') return;
     const list = createList(listName);
     newListInput.value = null;
     lists.push(list);
-    render();
+    saveAndRender(); 
 })
 
-function createList(name) {
+function createList(name) { //creating new 'todo'-list
     return {
         id: Date.now().toString(), name: name, tasks: []
     }
 }
+
+function save() {  
+    localStorage.setItem(LOCAl_STORAGE_LIST_KEY, JSON.stringify(lists));  //saving info in local storage
+    localStorage.setItem(LOCAL_STORAGE_SELECTED_LIST_ID_KEY, selectedListId); //
+    
+}
+
+function saveAndRender() { //rendering saved info from Local Storage
+    save();
+    render();
+}
+
+listsContainer.addEventListener('click', e => { //marking clicked list as active (selected) list
+    if (e.target.tagName.toLowerCase() === 'li') {
+        selectedListId = e.target.dataset.listId;
+        saveAndRender();
+    }
+})
