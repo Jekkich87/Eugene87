6b4d6f679d4877de9a715042495810b12daad27a
Jekkich87 <eugene87@ukr.net>
2020-04-07 13:24:03 +0300

    c training and some first progs

---
+++ C-C++/C-tasks/C-tasks-1/1) Первые программы/2.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+
+int main() {
+    printf("Wake up, Neo...\n\n");
+    printf("The Matrix has you...\n\n");
+    printf("Follow the white rabbit...\n");
+  // put your code here
+  return 0;
+}
---
+++ C-C++/C-tasks/C-tasks-1/1) Первые программы/3.c
@@ -0,0 +1,7 @@
+#include <stdio.h>
+
+int main() {
+    printf("NICKNAME: Neo\nCITY: St-Petersburg\nAGE: 35\n\HEIGHT: 180\nWEIGHT: 75\n");
+
+  return 0;
+}
