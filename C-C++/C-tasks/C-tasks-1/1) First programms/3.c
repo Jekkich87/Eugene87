e6c42a39dc7c542411f7e5f23136bee08311b017
Jekkich87 <eugene87@ukr.net>
2020-04-07 14:45:52 +0300

    translated tasks

--- C-C++/C-tasks/C-tasks-1/1) First programms/3.c
+++ C-C++/C-tasks/C-tasks-1/1) First programms/3.c
@@ -1,7 +1,7 @@
 #include <stdio.h>
 
 int main() {
-    printf("NICKNAME: Neo\nCITY: St-Petersburg\nAGE: 35\n\HEIGHT: 180\nWEIGHT: 75\n");
+    printf("NICKNAME: Neo\nCITY: New-York\nAGE: 35\n\HEIGHT: 180\nWEIGHT: 75\n");
 
   return 0;
 }
