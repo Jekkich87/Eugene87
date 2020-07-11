a01862af96da4b56e4fac453e9c0751d665d2a52
Jekkich87 <eugene87@ukr.net>
2020-07-11 12:01:34 +0300

    adjusting gamefield

--- C-C++/C-projects/Arkanoid/arkanoid.c
+++ C-C++/C-projects/Arkanoid/arkanoid.c
@@ -1,26 +1,53 @@
 #include<stdio.h>
 #include<conio.h>
+#include<stdlib.h>
+#include<string.h>
 
 #define width 65                //dimensions of gamefield
 #define height 25
 
-char field[height][width];      //defining gamefield
+char field[height][width+1];      //defining gamefield
 
 
 void initField(void);           // init gamefiled
-void showField(void);           //displaying gamefield
+void showField(void);           // displaying gamefield
+
 
 int main(void) {
+
+    initField();
+    showField();
+
     return 0;
+
 }
 
-void initField(void){     
+void initField(void){
+
     int i=0;
     for(i=0;i<width;i++){
-        field[0][i]='#';
+        field[0][i]='#';    
     }
+    field[0][width]='\0';
+
+    strncpy(field[1],field[0],width+1);
+    for(i=1;i<width-1;i++){
+        field[1][i]=' ';    
+    }
+
+    for(i=2;i<height-1;i++){
+        strncpy(field[i],field[1],width+1);
+    }
+
+
 }
 
-void showField(){       
-    printf("%s",field[0]);
+void showField(void){
+
+    int i=0;
+
+    for(i=0;i<height;i++){
+        printf("%s\n",field[i]);
+    }
+
 }
