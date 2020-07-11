07727723e36589f87fbf7a0d36121df07957befb
Jekkich87 <eugene87@ukr.net>
2020-07-11 11:12:58 +0300

    initiating gamefield

--- C-C++/C-projects/Arkanoid/arkanoid.c
+++ C-C++/C-projects/Arkanoid/arkanoid.c
@@ -1,7 +1,26 @@
 #include<stdio.h>
 #include<conio.h>
 
+#define width 65                //dimensions of gamefield
+#define height 25
+
+char field[height][width];      //defining gamefield
+
+
+void initField(void);           // init gamefiled
+void showField(void);           //displaying gamefield
 
 int main(void) {
     return 0;
+}
+
+void initField(void){     
+    int i=0;
+    for(i=0;i<width;i++){
+        field[0][i]='#';
+    }
+}
+
+void showField(){       
+    printf("%s",field[0]);
 }
