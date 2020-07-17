420475c5fba2ab19c4f49596f0453a29e89bb74d
Jekkich87 <eugene87@ukr.net>
2020-07-17 18:42:59 +0300

    creating player char, and placing it on map

--- C-C++/C-projects/Runner/runner.c
+++ C-C++/C-projects/Runner/runner.c
@@ -10,14 +10,28 @@
 
 char mapField[mapHeight][mapWidth+1];
 
+typedef struct SObject{
+    float x,y;
+}TObject;                                                   // coords of player;
+TObject mario;
+
+
 void initMap(void);
 void showMap(void);
+
+void setObjPos(TObject *obj,float xPos, float yPos);        // setting position for player;
+void putObjOnMap(TObject obj);                              // placing player on map
 
 int main(void){
 
     system("cls");
 
+    setObjPos(&mario,20,10);
+
     initMap();
+
+    putObjOnMap(mario);
+
     showMap();
 
 }
@@ -45,13 +59,23 @@ void showMap(void){
     mapField[mapHeight-1][mapWidth-1]='\0';
 
     for(j=0;j<mapHeight;j++){
-        printf("%s\n",mapField[i]);
+        printf("%s\n",mapField[j]);
     }
-
-
-    
-    
+ 
 }
 
+void setObjPos(TObject *obj,float xPos, float yPos){
 
+    (*obj).x=xPos;
+    (*obj).y=yPos;
+
+}
+
+void putObjOnMap(TObject obj){
+
+    int ix=(int)round(obj.x);
+    int iy=(int)round(obj.y);
+    mapField[iy][ix]='@';
+
+}
 
