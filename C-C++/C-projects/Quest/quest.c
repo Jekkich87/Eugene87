630597887064725d9e09134c111d03d64dcbb5da
Jekkich87 <eugene87@ukr.net>
2020-08-02 17:00:44 +0300

    map and it displaying
    drawed map;
    loading map from file;
    displaying map in console;

--- C-C++/C-projects/Quest/quest.c
+++ C-C++/C-projects/Quest/quest.c
@@ -6,6 +6,48 @@
 #include<stdbool.h>
 #include<math.h>
 
+#define width 80
+#define height 25
+
+struct{
+    char map[height][width+1];
+}loc;
+
+void loc_LoadFormFile(char* filename);              // function to loading location from file
+
 int main() {
+
+    system("cls");
+    system("mode 80,25");
+    system("mode con cols=80 lines=25");
+
+    loc_LoadFormFile("map_0_0.txt");
+    for(int i=0;i<height;i++){
+        printf("%s",loc.map[i]);
+    }
     return 0;
+}
+
+void loc_LoadFormFile(char* filename){
+
+    //memset(&loc.map,' ',sizeof(loc));           // filling in the map by space char;
+    for(int i=0;i<height;i++){
+        loc.map[i][width]='\0';
+    }
+    FILE* f=fopen(filename,"r");
+    char c[80];
+    int line=0;
+    while(!feof(f)){
+        fgets(c,width,f);
+        int cnt=strlen(c);
+        if(c[cnt]=='\n'){
+            cnt--;
+        }
+        strncpy(loc.map[line],c,cnt);
+        line++;
+        
+    }
+    fclose(f);
+
+    loc.map[height-1][width-1]='\0';    
 }
