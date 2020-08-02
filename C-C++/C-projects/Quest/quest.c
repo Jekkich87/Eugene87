c146d7049e9ad8340a531275be179095c529f76b
Jekkich87 <eugene87@ukr.net>
2020-08-02 17:40:00 +0300

    creating player
    creating player and placing it on the map

--- C-C++/C-projects/Quest/quest.c
+++ C-C++/C-projects/Quest/quest.c
@@ -13,24 +13,41 @@ struct{
     char map[height][width+1];
 }loc;
 
-void loc_LoadFormFile(char* filename);              // function to loading location from file
+struct{
+    POINT pos;
+}player;
 
-int main() {
+char map[height][width+1];
+
+void loc_LoadFormFile(char* filename);              // function to loading location from file
+void showMap(void);                                 // function for displaying map
+void loc_putOnMap(void);
+
+void initPlayer(int x, int y);
+void player_putOnMap(void);
+
+
+int main(void) {
 
     system("cls");
     system("mode 80,25");
     system("mode con cols=80 lines=25");
 
     loc_LoadFormFile("map_0_0.txt");
-    for(int i=0;i<height;i++){
-        printf("%s",loc.map[i]);
-    }
+    loc_putOnMap();
+
+    initPlayer(5,5);
+    player_putOnMap();
+    
+    showMap();
+
+
     return 0;
 }
 
 void loc_LoadFormFile(char* filename){
 
-    //memset(&loc.map,' ',sizeof(loc));           // filling in the map by space char;
+    //memset(&loc.map,' ',sizeof(loc));            // filling in the map by space char;
     for(int i=0;i<height;i++){
         loc.map[i][width]='\0';
     }
@@ -50,4 +67,23 @@ void loc_LoadFormFile(char* filename){
     fclose(f);
 
     loc.map[height-1][width-1]='\0';    
+}
+
+void showMap(void){
+    for(int i=0;i<height;i++){
+        printf("%s",map[i]);
+    }
+}
+
+void loc_putOnMap(void){
+    memcpy(map,loc.map,sizeof(map));
+}
+
+void initPlayer(int x, int y){
+    player.pos.x=x;
+    player.pos.y=y;
+}
+
+void player_putOnMap(void){
+    map[player.pos.y][player.pos.x]='A';
 }
