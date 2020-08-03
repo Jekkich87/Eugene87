5b6d84cafded65bc2ddf76c49a10dd524224d459
Jekkich87 <eugene87@ukr.net>
2020-08-03 20:12:23 +0300

    control and player position
    added contol that player can move;
    added save and load position that player can continue from place, where he exited from game

--- C-C++/C-projects/Quest/quest.c
+++ C-C++/C-projects/Quest/quest.c
@@ -14,6 +14,7 @@ struct{
 }loc;
 
 struct{
+    char name[20];
     POINT pos;
 }player;
 
@@ -23,24 +24,37 @@ void loc_LoadFormFile(char* filename);              // function to loading locat
 void showMap(void);                                 // function for displaying map
 void loc_putOnMap(void);
 
-void initPlayer(int x, int y);
+void initPlayer(int x, int y,char* name);
 void player_putOnMap(void);
+
+void player_Control(void);
+void player_SavePos(void);
+void player_LoadPos(char* name);
+
+void setCursor(int x,int y);
+void hideCursor(void);
 
 
 int main(void) {
 
+    
     system("cls");
     system("mode 80,25");
     system("mode con cols=80 lines=25");
 
     loc_LoadFormFile("map_0_0.txt");
-    loc_putOnMap();
+    player_LoadPos("Player");
 
-    initPlayer(5,5);
-    player_putOnMap();
-    
-    showMap();
+    do{
+        player_Control();
+        loc_putOnMap();      
+        player_putOnMap();
+        showMap();
+        Sleep(50);
 
+    }while(GetKeyState(VK_ESCAPE)>=0);
+
+    player_SavePos();
 
     return 0;
 }
@@ -70,6 +84,8 @@ void loc_LoadFormFile(char* filename){
 }
 
 void showMap(void){
+    setCursor(0,0);
+
     for(int i=0;i<height;i++){
         printf("%s",map[i]);
     }
@@ -79,11 +95,62 @@ void loc_putOnMap(void){
     memcpy(map,loc.map,sizeof(map));
 }
 
-void initPlayer(int x, int y){
+void initPlayer(int x, int y,char* name){
     player.pos.x=x;
     player.pos.y=y;
+    sprintf(player.name,name);
 }
 
 void player_putOnMap(void){
     map[player.pos.y][player.pos.x]='A';
+}
+
+void player_Control(void){
+
+    POINT old=player.pos;
+    if(GetKeyState(VK_UP)<0){player.pos.y--;}
+    if(GetKeyState(VK_DOWN)<0){player.pos.y++;}
+    if(GetKeyState(VK_LEFT)<0){player.pos.x--;}
+    if(GetKeyState(VK_RIGHT)<0){player.pos.x++;}
+    if(map[player.pos.y][player.pos.x]!=' '){
+        player.pos=old;
+    }
+}
+
+void player_SavePos(void){
+
+    FILE* f=fopen(player.name,"wb");
+    fwrite(&player,1,sizeof(player),f);
+    fclose(f);
+
+}
+
+void player_LoadPos(char* name){
+    FILE* f=fopen(name,"rb");
+    if(f==NULL){
+        initPlayer(5,5,name);
+    }
+    else{
+        fread(&player,1,sizeof(player),f);
+    }
+    fclose(f);
+}
+
+void setCursor(int x,int y){
+
+    COORD coord;
+    coord.X=x;
+    coord.Y=y;
+    SetConsoleCursorPosition(GetStdHandle(STD_OUTPUT_HANDLE),coord);
+    //Sleep(10);
+
+}
+
+void hideCursor(void){
+
+    CONSOLE_CURSOR_INFO CCI;
+    CCI.bVisible=false;
+    CCI.dwSize=1;
+    SetConsoleCursorInfo(GetStdHandle(STD_OUTPUT_HANDLE),&CCI);
+
 }
