538e07f92ea9d306fc6a41b351ca7830f5b7a44c
Jekkich87 <55157052+Jekkich87@users.noreply.github.com>
2021-01-07 19:33:33 +0200

    Adding properties
    Adding properties to the TodoModel.
    
    TodoModelisa class wich contains properties of grid fields.

---
+++ ToDoList/ToDoList/Models/TodoModel.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ToDoList.Models
+{
+    class TodoModel
+    {
+        public DateTime CreationDate { get; set; } = DateTime.Now;
+
+        private bool _isDone;
+
+        public bool IsDone
+        {
+            get { return _isDone; }
+            set { _isDone = value; }
+        }
+
+        private string _notes;
+
+        public string _Notes
+        {
+            get { return _notes; }
+            set { _notes = value; }
+        }
+    }
+}
